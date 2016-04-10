57fda8307544b91f153a65a30445034f3ae9615c
bhSPARSE <weifeng.liu@nbi.ku.dk>
2016-04-10 14:30:50 +0200

    Update anonymouslib_cuda.h

--- CSR5_cuda/anonymouslib_cuda.h
+++ CSR5_cuda/anonymouslib_cuda.h
@@ -178,7 +178,7 @@ int anonymouslibHandle<ANONYMOUSLIB_IT, ANONYMOUSLIB_UIT, ANONYMOUSLIB_VT>::asCS
         if (err != ANONYMOUSLIB_SUCCESS)
             return ANONYMOUSLIB_CSR_TO_CSR5_FAILED;
         cudaDeviceSynchronize();
-        tile_desc_time += tile_desc_timer.stop();
+        //tile_desc_time += tile_desc_timer.stop(); // comment this for avoiding a bug in CUDA 7.x
 
         if (_num_offsets)
         {
