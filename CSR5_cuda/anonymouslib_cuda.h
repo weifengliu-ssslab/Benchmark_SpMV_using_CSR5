b79e403a2fc19166574a3558c5f6236fa5f283cf
bhSPARSE <weifeng.liu@nbi.ku.dk>
2016-04-11 07:10:25 +0200

    Update anonymouslib_cuda.h

--- CSR5_cuda/anonymouslib_cuda.h
+++ CSR5_cuda/anonymouslib_cuda.h
@@ -171,6 +171,7 @@ int anonymouslibHandle<ANONYMOUSLIB_IT, ANONYMOUSLIB_UIT, ANONYMOUSLIB_VT>::asCS
         // step 2. generate partition descriptor
 
         _num_offsets = 0;
+        tile_desc_timer.start();
         err = generate_partition_descriptor(_csr5_sigma, _p, _m,
                                             _bit_y_offset, _bit_scansum_offset, _num_packet,
                                             _csr_row_pointer, _csr5_partition_pointer, _csr5_partition_descriptor,
@@ -178,7 +179,7 @@ int anonymouslibHandle<ANONYMOUSLIB_IT, ANONYMOUSLIB_UIT, ANONYMOUSLIB_VT>::asCS
         if (err != ANONYMOUSLIB_SUCCESS)
             return ANONYMOUSLIB_CSR_TO_CSR5_FAILED;
         cudaDeviceSynchronize();
-        //tile_desc_time += tile_desc_timer.stop(); // comment this for avoiding a bug in CUDA 7.x
+        tile_desc_time += tile_desc_timer.stop(); // comment this for avoiding a bug in CUDA 7.x
 
         if (_num_offsets)
         {
